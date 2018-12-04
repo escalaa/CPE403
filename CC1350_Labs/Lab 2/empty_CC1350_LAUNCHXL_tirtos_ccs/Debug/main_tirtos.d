b6daa2ee2c79571d4eadc066c86b5bd3eda0e427
escalaa <36839256+escalaa@users.noreply.github.com>
2018-12-04 00:58:23 -0800

    CC1350 Lab 2

---
+++ CC1350_Labs/Lab 2/empty_CC1350_LAUNCHXL_tirtos_ccs/Debug/main_tirtos.d
@@ -0,0 +1,199 @@
+# FIXED
+
+main_tirtos.obj: ../main_tirtos.c
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs/pthread.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs/sys/types.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stddef.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs/sys/_internal.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/../include/time.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/_ti_config.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/linkage.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/abi_prefix.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/xlocale/_time.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs/time.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs/signal.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/../include/signal.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/../include/sys/timespec.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_timespec.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs/sched.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/sysbios/BIOS.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/std.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdarg.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/targets/arm/elf/std.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/targets/arm/elf/M3.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/targets/std.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/xdc.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types__prologue.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/package/package.defs.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types__epilogue.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/sysbios/BIOS__prologue.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/sysbios/package/package.defs.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Error.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Error__prologue.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Main.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IModule.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IHeap.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IInstance.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IModule.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Error.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Memory.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IModule.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IHeap.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Error.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/package/Memory_HeapProxy.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IInstance.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IHeap.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Error.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IGateProvider.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IInstance.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IModule.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/package/Main_Module_GateProxy.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IInstance.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IGateProvider.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IModule.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Error__epilogue.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IModule.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IGateProvider.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/sysbios/package/BIOS_RtsGateProxy.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IInstance.h
+main_tirtos.obj: D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IGateProvider.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/sysbios/BIOS__epilogue.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/sysbios/package/BIOS_RtsGateProxy.h
+main_tirtos.obj: ../Board.h
+main_tirtos.obj: ../CC1350_LAUNCHXL.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/drivers/PIN.h
+main_tirtos.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdbool.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/ioc.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_types.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/../inc/hw_chip_def.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_memmap.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_ioc.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_ints.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/interrupt.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_nvic.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/debug.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/cpu.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_cpu_scs.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../driverlib/rom.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/gpio.h
+main_tirtos.obj: C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_gpio.h
+
+../main_tirtos.c: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs/pthread.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs/sys/types.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stddef.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs/sys/_internal.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/../include/time.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/_ti_config.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/linkage.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/abi_prefix.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/xlocale/_time.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs/time.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs/signal.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/../include/signal.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/../include/sys/timespec.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_timespec.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/posix/ccs/sched.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/sysbios/BIOS.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/std.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdarg.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/targets/arm/elf/std.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/targets/arm/elf/M3.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/targets/std.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/xdc.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types__prologue.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/package/package.defs.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types__epilogue.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/sysbios/BIOS__prologue.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/sysbios/package/package.defs.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Error.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Error__prologue.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Main.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IModule.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IHeap.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IInstance.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IModule.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Error.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Memory.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IModule.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IHeap.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Error.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/package/Memory_HeapProxy.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IInstance.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IHeap.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Error.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IGateProvider.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IInstance.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IModule.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/package/Main_Module_GateProxy.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IInstance.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IGateProvider.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IModule.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Error__epilogue.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IModule.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IGateProvider.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/sysbios/package/BIOS_RtsGateProxy.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/Types.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IInstance.h: 
+D:/Coding/CCS8/xdctools_3_50_08_24_core/packages/xdc/runtime/IGateProvider.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/sysbios/BIOS__epilogue.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/kernel/tirtos/packages/ti/sysbios/package/BIOS_RtsGateProxy.h: 
+../Board.h: 
+../CC1350_LAUNCHXL.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/drivers/PIN.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdbool.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/ioc.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_types.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/../inc/hw_chip_def.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_memmap.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_ioc.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_ints.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/interrupt.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_nvic.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/debug.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/cpu.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_cpu_scs.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../driverlib/rom.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/gpio.h: 
+C:/ti/simplelink_cc13x0_sdk_2_30_00_20/source/ti/devices/cc13x0/driverlib/../inc/hw_gpio.h: 
