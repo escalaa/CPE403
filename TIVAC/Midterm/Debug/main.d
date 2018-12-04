57084045930b318c88fe819f4124a1dcdd539645
escalaa <36839256+escalaa@users.noreply.github.com>
2018-12-04 00:16:56 -0800

    Midterm TivaC

---
+++ TIVAC/Midterm/Debug/main.d
@@ -0,0 +1,63 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdarg.h
+main.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_types.h
+main.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/cdefs.h
+main.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_types.h
+main.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdbool.h
+main.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdint.h
+main.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/stdint.h
+main.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_stdint.h
+main.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_stdint.h
+main.obj: D:/Coding/CCS8/TivaC123/inc/tm4c123gh6pm.h
+main.obj: D:/Coding/CCS8/TivaC123/inc/hw_i2c.h
+main.obj: D:/Coding/CCS8/TivaC123/inc/hw_memmap.h
+main.obj: D:/Coding/CCS8/TivaC123/inc/hw_types.h
+main.obj: D:/Coding/CCS8/TivaC123/inc/hw_gpio.h
+main.obj: D:/Coding/CCS8/TivaC123/driverlib/i2c.h
+main.obj: D:/Coding/CCS8/TivaC123/driverlib/sysctl.h
+main.obj: D:/Coding/CCS8/TivaC123/driverlib/gpio.h
+main.obj: D:/Coding/CCS8/TivaC123/driverlib/pin_map.h
+main.obj: D:/Coding/CCS8/TivaC123/driverlib/uart.h
+main.obj: D:/Coding/CCS8/TivaC123/utils/uartstdio.h
+main.obj: D:/Coding/CCS8/TivaC123/driverlib/interrupt.h
+main.obj: D:/Coding/CCS8/TivaC123/driverlib/hibernate.h
+main.obj: ../TSL2591_def.h
+main.obj: ../ustdlib.h
+main.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/time.h
+main.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/_ti_config.h
+main.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/linkage.h
+main.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/abi_prefix.h
+main.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/xlocale/_time.h
+
+../main.c: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdarg.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_types.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/cdefs.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_types.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdbool.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdint.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/stdint.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_stdint.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_stdint.h: 
+D:/Coding/CCS8/TivaC123/inc/tm4c123gh6pm.h: 
+D:/Coding/CCS8/TivaC123/inc/hw_i2c.h: 
+D:/Coding/CCS8/TivaC123/inc/hw_memmap.h: 
+D:/Coding/CCS8/TivaC123/inc/hw_types.h: 
+D:/Coding/CCS8/TivaC123/inc/hw_gpio.h: 
+D:/Coding/CCS8/TivaC123/driverlib/i2c.h: 
+D:/Coding/CCS8/TivaC123/driverlib/sysctl.h: 
+D:/Coding/CCS8/TivaC123/driverlib/gpio.h: 
+D:/Coding/CCS8/TivaC123/driverlib/pin_map.h: 
+D:/Coding/CCS8/TivaC123/driverlib/uart.h: 
+D:/Coding/CCS8/TivaC123/utils/uartstdio.h: 
+D:/Coding/CCS8/TivaC123/driverlib/interrupt.h: 
+D:/Coding/CCS8/TivaC123/driverlib/hibernate.h: 
+../TSL2591_def.h: 
+../ustdlib.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/time.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/_ti_config.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/linkage.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/abi_prefix.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/xlocale/_time.h: 
---
+++ TIVAC/Midterm/Debug/ustdlib.d
@@ -0,0 +1,35 @@
+# FIXED
+
+ustdlib.obj: ../ustdlib.c
+ustdlib.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdint.h
+ustdlib.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/stdint.h
+ustdlib.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/cdefs.h
+ustdlib.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_types.h
+ustdlib.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_types.h
+ustdlib.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_stdint.h
+ustdlib.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_stdint.h
+ustdlib.obj: D:/Coding/CCS8/TivaC123/driverlib/debug.h
+ustdlib.obj: D:/Coding/CCS8/TivaC123/utils/ustdlib.h
+ustdlib.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdarg.h
+ustdlib.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/time.h
+ustdlib.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/_ti_config.h
+ustdlib.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/linkage.h
+ustdlib.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/abi_prefix.h
+ustdlib.obj: D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/xlocale/_time.h
+
+../ustdlib.c: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdint.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/stdint.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/cdefs.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_types.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_types.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_stdint.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_stdint.h: 
+D:/Coding/CCS8/TivaC123/driverlib/debug.h: 
+D:/Coding/CCS8/TivaC123/utils/ustdlib.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdarg.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/time.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/_ti_config.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/linkage.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/abi_prefix.h: 
+D:/Coding/CCS8/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/xlocale/_time.h: 
